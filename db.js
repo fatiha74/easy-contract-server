7dc13e966fafb1887c0b3649712926548e28656f
fatiha Abellaoui <fatiha.abdellaoui74@gmail.com>
2023-01-10 16:34:34 +0100

    c

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const Pool = require("pg").Pool;
 // require('dotenv').config();
 
 const pool = new Pool({
-    user: "fatiha",
+    user: 'fatiha',
     // user: "postgres",
      password: process.env.BDD_PASSWORD,
     // password:"FZRATdB65po3gh0AOJxt6lfbBCXf2Lfk",
-    host: "dpg-ceqp8u02i3mov0ikm0eg-a.frankfurt-postgres.render.com",
+    host: 'dpg-ceqp8u02i3mov0ikm0eg-a.frankfurt-postgres.render.com',
 
     //  port: process.env.PORT,
      port:5432,
