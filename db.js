65d56d275868ec3879c333ee86563b9f596c0909
fatiha Abellaoui <fatiha.abdellaoui74@gmail.com>
2023-01-10 16:25:40 +0100

    c

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "fatiha",
     // user: "postgres",
-     password: process.env.BDD_password,
-    password:"FZRATdB65po3gh0AOJxt6lfbBCXf2Lfk",
+     password: process.env.BDD_PASSWORD,
+    // password:"FZRATdB65po3gh0AOJxt6lfbBCXf2Lfk",
     host: "dpg-ceqp8u02i3mov0ikm0eg-a.frankfurt-postgres.render.com",
 
     //  port: process.env.PORT,
